1891c4a7fce3b3baa34172e0c7e9a07b9893b88d
Fendor96 <axnod50@gmail.com>
2025-05-04 20:53:58 +0100

    PREMIEIR COMIT

---
+++ 500.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kin Webb - ERREUR 404 !</title>
+    <style>
+
+        .divider{
+            width: 90%;
+            margin: 10px auto;
+            background-color: #db3a00;
+            height: 0.3vh;
+        }
+
+        header{
+            display: flex;
+            justify-content: center;
+        }
+
+        header a{
+            text-decoration: none;
+            color: #db3a00;
+            font-size: 30px;
+        }
+
+        section{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+
+        h1{
+            font-size: 50px;
+            color: #db3a00;
+            text-align: center;
+        }
+
+        body{
+            font-family: "Montserrat", serif;
+        }
+
+        .img{
+            background: url('../Styles/assets/Mining\ Sugar\ 🍭.gif');
+            background-position: center;
+            background-size: cover;
+            background-repeat: no-repeat;
+            width: 300px;
+            height: 300px;
+            border-radius: 50%;
+            margin: 10px auto;
+        }
+
+
+    </style>
+</head>
+<body>
+    <header>
+        <nav><a href="../index.html">KIN MENU</a></nav>
+    </header>
+
+    <div class="divider"></div>
+
+    <section>
+        <h1>500</h1>
+        <p>Oups! il s'est passé une erreur de notre côté.</p>
+        <a href="../index.html">Retour à l'acceuil</a>
+
+        <div class="img"></div>
+    </section>
+</body>
+</html>
